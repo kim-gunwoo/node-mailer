3ca74ed029fc8281e650c99e7159427d8ad75a43
git-k <kgunu93@gmail.com>
2020-10-28 21:44:48 +0900

    템플릿 수정

--- dev_template.js
+++ dev_template.js
@@ -1,4 +1,7 @@
 module.exports = {
+    NODEMAILER_SERVICE: "메일서버 이름 ex) gmail, naver",
+    NODEMAILER_HOST: "메일서버 주소 ex) smtp.naver.com",
+    NODEMAILER_PORT: "메일서버 사용 포트 ex) 465",
     NODEMAILER_USER: "유저 메일",
     NODEMAILER_PASS: "유저 패스워드",
 };
